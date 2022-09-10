bb37d6f7856ee7a3ceea8d749f9ab6723698344d
Mateusz Barnacki <barnackimateusz@gmail.com>
2022-09-10 19:10:17 +0200

    Remove hours from timer

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -7,11 +7,9 @@ interface TimerProps {
 
 export function mapSeconds(secondsCounter: number): string {
     const minutesNumber: number = Math.floor(secondsCounter / 60);
-    const hoursNumber: number = Math.floor(secondsCounter / 3600);
     const seconds: string = (secondsCounter % 60) > 9 ? '' + (secondsCounter % 60) : '0' + (secondsCounter % 60);
     const minutes: string = minutesNumber > 9 ? '' + minutesNumber : '0' + minutesNumber;
-    const hours: string = hoursNumber > 9 ? '' + hoursNumber : '0' + hoursNumber;
-    return hours + ':' + minutes + ':' + seconds;
+    return minutes + ':' + seconds;
 }
 
 const Timer: React.FC<TimerProps> = ({seconds, countTime}) => {
